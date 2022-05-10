1a8e7127b4794d455f8bf0bc9340f886360515f7
Ula <1295593@mail.ru>
2022-05-10 15:27:05 +0800

    Edits

--- LoginApp/ViewControllers/HobbyViewController.swift
+++ LoginApp/ViewControllers/HobbyViewController.swift
@@ -16,8 +16,8 @@ class HobbyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         hobbyLabel.text = hobby.person.hobby
-//        navigationItem.backBarButtonItem?.tintColor = UIColor(red: 150, green: 170, blue: 12, alpha: 1)
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let photoVC = segue.destination as? PhotosViewController else { return }
         photoVC.photo = hobby
